db88996920336f7486eb5f338421fa5142227c4c
Daniel Pettersson <profil@errno.se>
2015-03-12 15:50:12 +0100

    Ekeroot är dum

--- Testrunner.hs
+++ Testrunner.hs
@@ -61,22 +61,21 @@ runTest (Test name testData) = do
   removeFile $ tp ++ name ++ ".js"
   putStrLn ""
 
-newTest :: (Show a) => Name -> IO [a] -> IO Test 
+newTest :: (Show a) => Name -> IO [a] -> IO Test
 newTest name td = (\t -> Test name $ show t) <$> td
- 
-testList :: IO [Test]
-testList = 
-  sequence [ newTest "Addition" (sample' (arbitrary :: Gen (Double, Double))) 
-           , newTest "Addition" (sample' (arbitrary :: Gen (Int, Int))) 
-           , newTest "Addition" (sample' (arbitrary :: Gen (Int, Double))) 
-           , newTest "BitOps"   (sample' (arbitrary :: Gen (Word, Word))) 
-           , newTest "ComplexFromIntegral" (sample' (arbitrary :: Gen (Int, Int, Int, Int))) 
-           , newTest "ComplexFromIntegral" (sample' (arbitrary :: Gen (Double, Double, Double, Double))) 
-           , newTest "Elem"     (sample' (arbitrary :: Gen (String, String))) 
-           , newTest "Int64"    (sample' (arbitrary :: Gen (Int64, Int64))) 
-           , newTest "DoubleConversion" (sample' (arbitrary :: Gen (Int))) 
 
-           ]
+testList :: IO [Test]
+testList = sequence
+  [ newTest "Addition" (sample' (arbitrary :: Gen (Double, Double)))
+  , newTest "Addition" (sample' (arbitrary :: Gen (Int, Int)))
+  , newTest "Addition" (sample' (arbitrary :: Gen (Int, Double)))
+  , newTest "BitOps"   (sample' (arbitrary :: Gen (Word, Word)))
+  , newTest "ComplexFromIntegral" (sample' (arbitrary :: Gen (Int, Int, Int, Int)))
+  , newTest "ComplexFromIntegral" (sample' (arbitrary :: Gen (Double, Double, Double, Double)))
+  , newTest "Elem"     (sample' (arbitrary :: Gen (String, String)))
+  , newTest "Int64"    (sample' (arbitrary :: Gen (Int64, Int64)))
+  , newTest "DoubleConversion" (sample' (arbitrary :: Gen (Int)))
+  ]
 
 main :: IO ()
 main = do
